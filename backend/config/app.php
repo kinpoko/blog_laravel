03c39a293445e6fdaf8554cdd64418bcb4048932
kinpoko <79001545+kinpoko@users.noreply.github.com>
2021-03-06 06:10:51 +0900

    heroku error

--- backend/config/app.php
+++ backend/config/app.php
@@ -14,6 +14,7 @@ return [
     */
 
     'name' => env('APP_NAME', 'Laravel'),
+    'debug' => env('APP_DEBUG', true),
     /*
     |--------------------------------------------------------------------------
     | Application Environment
