3d666d6b3721934d349f173e4ee5ba444c05160e
kinpoko <79001545+kinpoko@users.noreply.github.com>
2021-03-06 05:55:54 +0900

    deploy to heroku 2

--- backend/config/app.php
+++ backend/config/app.php
@@ -14,7 +14,6 @@ return [
     */
 
     'name' => env('APP_NAME', 'Laravel'),
-    'log' => 'errorlog',
     /*
     |--------------------------------------------------------------------------
     | Application Environment
