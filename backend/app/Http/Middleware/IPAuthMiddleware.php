51220b5a9bb7f7ffc39f825902a7bf74a4a9c6fa
kinpoko <79001545+kinpoko@users.noreply.github.com>
2021-04-06 23:44:45 +0900

    fix

--- backend/app/Http/Middleware/IPAuthMiddleware.php
+++ backend/app/Http/Middleware/IPAuthMiddleware.php
@@ -21,7 +21,7 @@ class IPAuthMiddleware
         $isDenied = !$this->whetherThisIpAccepted();
     
         if ($isPrd && $isDenied) {
-            return \Response::create(view("errors.403"), 403);
+            return abort(403);
         }
     
         return $next($request);
